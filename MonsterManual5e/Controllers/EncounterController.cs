bd61f867d07e9fab5080cfe5cae0ba1b27d483c5
ocengineer <corbin.murrow@gmail.com>
2017-09-21 14:19:30 -0500

    Encounters framework

---
+++ MonsterManual5e/Controllers/EncounterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonsterManual5e.Views.Encounter
+{
+    public class EncounterController : Controller
+    {
+        // GET: Encounter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add(int id)
+        {
+            return null;
+        }
+
+        public ActionResult DifficultyCalculator(int id)
+        {
+            return null;
+        }
+    }
+}
